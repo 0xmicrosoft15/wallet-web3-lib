web3-wallet/web3-wallet

refactor: package structures

packages/ethereum/src/createStore.ts
@@ -1,0 +1,71 @@
+import create from 'zustand/vanilla';
+
+import type { Actions, State, Store } from './types';
+
+const DEFAULT_STATE: State = {
+  chainId: undefined,
+  accounts: undefined,
+  isActivating: false,
+};
+
+export const createStore = (): {
+  store: Store;
+  actions: Actions;
+} => {
+  const store = create<State>()(() => DEFAULT_STATE);
+
+  // flag for tracking updates so we don't clobber data when cancelling activation
+  let nullifier = 0;
+
+  /**
+   * Sets activating to true, indicating that an update is in progress.
+   *
+   * @returns cancelActivation - A function that cancels the activation by setting activating to false,
+   * as long as there haven't been any intervening updates.
+   */
+  function startActivation(): () => void {
+    const nullifierCached = ++nullifier;
+
+    store.setState({ isActivating: true });
+
+    // return a function that cancels the activation iff nothing else has happened
+    return () => {
+      if (nullifier === nullifierCached)
+        store.setState({ isActivating: false });
+    };
+  }
+
+  /**
+   * Used to report a `stateUpdate` which is merged with existing state. The first `stateUpdate` that results in chainId
+   * and accounts being set will also set activating to false, indicating a successful connection.
+   *
+   * @param stateUpdate - The state update to report.
+   */
+  function update(stateUpdate: Partial<State>): void {
+    nullifier++;
+
+    store.setState((existingState): State => {
+      // determine the next chainId and accounts
+      const chainId = stateUpdate.chainId ?? existingState.chainId;
+      const accounts = stateUpdate.accounts ?? existingState.accounts;
+
+      // ensure that the activating flag is cleared when appropriate
+      let activating = existingState.isActivating;
+      if (activating && chainId && accounts) {
+        activating = false;
+      }
+
+      return { chainId, accounts, isActivating: activating };
+    });
+  }
+
+  /**
+   * Resets connector state back to the default state.
+   */
+  function resetState(): void {
+    nullifier++;
+    store.setState(DEFAULT_STATE);
+  }
+
+  return { store, actions: { startActivation, update, resetState } };
+};

packages/ethereum/src/types.ts
@@ -1,0 +1,93 @@
+import type { Networkish } from '@ethersproject/networks';
+import type { BaseProvider as EthersBaseProvider } from '@ethersproject/providers';
+import type {
+  Actions as BaseActions,
+  Hooks as BaseHooks,
+  Provider as BaseProvider,
+  State as BaseState,
+  Store as BaseStore,
+  Wallet as BaseWallet,
+} from '@web3-wallet/types';
+import { Connector as BaseConnector } from '@web3-wallet/types';
+import type { EventEmitter } from 'node:events';
+
+export interface State extends BaseState {
+  chainId?: number;
+}
+
+export interface Hooks extends BaseHooks<State> {
+  useAccount: () => string | undefined;
+  useIsActive: () => boolean;
+  useProvider: (
+    network?: Networkish,
+    enabled?: boolean,
+  ) => EthersBaseProvider | undefined;
+  useENSNames: (
+    provider?: EthersBaseProvider,
+  ) => undefined[] | (string | null)[];
+  useENSName: (provider?: EthersBaseProvider) => undefined | string | null;
+}
+
+export type Store = BaseStore<State>;
+
+export type Actions = BaseActions<State>;
+
+// per EIP-1193
+export interface RequestArguments {
+  readonly method: string;
+  readonly params?: readonly unknown[] | object;
+}
+
+// per EIP-1193
+export interface ProviderRpcError extends Error {
+  message: string;
+  code: number;
+  data?: unknown;
+}
+
+// per EIP-1193
+export interface Provider extends EventEmitter, BaseProvider {
+  request<T>(args: RequestArguments): Promise<T>;
+}
+
+// per EIP-1193
+export interface ProviderConnectInfo {
+  readonly chainId: string;
+}
+
+export class ProviderNoFoundError extends Error {
+  public constructor(message = 'Provider not found') {
+    super(message);
+    this.name = ProviderNoFoundError.name;
+    Object.setPrototypeOf(this, ProviderNoFoundError.prototype);
+  }
+}
+
+// per EIP-3085
+export interface AddEthereumChainParameter {
+  chainId: number;
+  chainName: string;
+  nativeCurrency: {
+    name: string;
+    symbol: string; // 2-6 characters long
+    decimals: 18;
+  };
+  rpcUrls: string[];
+  blockExplorerUrls?: string[];
+  iconUrls?: string[]; // Currently ignored.
+}
+
+// per EIP-747
+export interface WatchAssetParameters {
+  address: string; // The address that the token is at.
+  symbol: string; // A ticker symbol or shorthand, up to 5 chars.
+  decimals: number; // The number of decimals in the token
+  image: string; // A string url of the token logo
+}
+
+export abstract class Connector extends BaseConnector<Provider, State> {}
+export type Wallet<C extends Connector = Connector> = BaseWallet<
+  State,
+  Hooks,
+  C
+>;

0xmicrosoft15/wallet-web3-lib

packages/ethereum/src/createStore.ts
@@ -1,0 +1,71 @@
+import create from 'zustand/vanilla';
+
+import type { Actions, State, Store } from './types';
+
+const DEFAULT_STATE: State = {
+  chainId: undefined,
+  accounts: undefined,
+  isActivating: false,
+};
+
+export const createStore = (): {
+  store: Store;
+  actions: Actions;
+} => {
+  const store = create<State>()(() => DEFAULT_STATE);
+
+  // flag for tracking updates so we don't clobber data when cancelling activation
+  let nullifier = 0;
+
+  /**
+   * Sets activating to true, indicating that an update is in progress.
+   *
+   * @returns cancelActivation - A function that cancels the activation by setting activating to false,
+   * as long as there haven't been any intervening updates.
+   */
+  function startActivation(): () => void {
+    const nullifierCached = ++nullifier;
+
+    store.setState({ isActivating: true });
+
+    // return a function that cancels the activation iff nothing else has happened
+    return () => {
+      if (nullifier === nullifierCached)
+        store.setState({ isActivating: false });
+    };
+  }
+
+  /**
+   * Used to report a `stateUpdate` which is merged with existing state. The first `stateUpdate` that results in chainId
+   * and accounts being set will also set activating to false, indicating a successful connection.
+   *
+   * @param stateUpdate - The state update to report.
+   */
+  function update(stateUpdate: Partial<State>): void {
+    nullifier++;
+
+    store.setState((existingState): State => {
+      // determine the next chainId and accounts
+      const chainId = stateUpdate.chainId ?? existingState.chainId;
+      const accounts = stateUpdate.accounts ?? existingState.accounts;
+
+      // ensure that the activating flag is cleared when appropriate
+      let isActivating = existingState.isActivating;
+      if (isActivating && chainId && accounts) {
+        isActivating = false;
+      }
+
+      return { chainId, accounts, isActivating };
+    });
+  }
+
+  /**
+   * Resets connector state back to the default state.
+   */
+  function resetState(): void {
+    nullifier++;
+    store.setState(DEFAULT_STATE);
+  }
+
+  return { store, actions: { startActivation, update, resetState } };
+};

packages/ethereum/src/types.ts
@@ -1,0 +1,93 @@
+import type { Networkish } from '@ethersproject/networks';
+import type { BaseProvider as EthersBaseProvider } from '@ethersproject/providers';
+import type {
+  Actions as BaseActions,
+  Hooks as BaseHooks,
+  Provider as BaseProvider,
+  State as BaseState,
+  Store as BaseStore,
+  Wallet as BaseWallet,
+} from '@web3-wallet/types';
+import { Connector as BaseConnector } from '@web3-wallet/types';
+import type { EventEmitter } from 'node:events';
+
+export interface State extends BaseState {
+  chainId?: number;
+}
+
+export interface Hooks extends BaseHooks<State> {
+  useAccount: () => string | undefined;
+  useIsActive: () => boolean;
+  useProvider: (
+    network?: Networkish,
+    enabled?: boolean,
+  ) => EthersBaseProvider | undefined;
+  useENSNames: (
+    provider?: EthersBaseProvider,
+  ) => undefined[] | (string | null)[];
+  useENSName: (provider?: EthersBaseProvider) => undefined | string | null;
+}
+
+export type Store = BaseStore<State>;
+
+export type Actions = BaseActions<State>;
+
+// per EIP-1193
+export interface RequestArguments {
+  readonly method: string;
+  readonly params?: readonly unknown[] | object;
+}
+
+// per EIP-1193
+export interface ProviderRpcError extends Error {
+  message: string;
+  code: number;
+  data?: unknown;
+}
+
+// per EIP-1193
+export interface Provider extends EventEmitter, BaseProvider {
+  request<T>(args: RequestArguments): Promise<T>;
+}
+
+// per EIP-1193
+export interface ProviderConnectInfo {
+  readonly chainId: string;
+}
+
+export class ProviderNoFoundError extends Error {
+  public constructor(message = 'Provider not found') {
+    super(message);
+    this.name = ProviderNoFoundError.name;
+    Object.setPrototypeOf(this, ProviderNoFoundError.prototype);
+  }
+}
+
+// per EIP-3085
+export interface AddEthereumChainParameter {
+  chainId: number;
+  chainName: string;
+  nativeCurrency: {
+    name: string;
+    symbol: string; // 2-6 characters long
+    decimals: 18;
+  };
+  rpcUrls: string[];
+  blockExplorerUrls?: string[];
+  iconUrls?: string[]; // Currently ignored.
+}
+
+// per EIP-747
+export interface WatchAssetParameters {
+  address: string; // The address that the token is at.
+  symbol: string; // A ticker symbol or shorthand, up to 5 chars.
+  decimals: number; // The number of decimals in the token
+  image: string; // A string url of the token logo
+}
+
+export abstract class Connector extends BaseConnector<Provider, State> {}
+export type Wallet<T extends Connector = Connector> = BaseWallet<
+  T,
+  State,
+  Hooks
+>;